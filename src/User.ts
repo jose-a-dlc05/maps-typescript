d7bfedc9a9b1808f28cd700e69725a8464524bcf
Jose De La Cruz <jose.delcruz0490@icloud.com>
2021-11-06 13:11:58 -0400

    Added faker library to the User class file

--- src/User.ts
+++ src/User.ts
@@ -1,1 +1,21 @@
-class User {}
+import faker from 'faker';
+
+class User {
+	name: string;
+	location: {
+		lat: number;
+		lng: number;
+	};
+
+	constructor() {
+		this.name = faker.name.firstName();
+		this.location = {
+			lat: parseFloat(faker.address.latitude()),
+			lng: parseFloat(faker.address.longitude()),
+		};
+	}
+}
+
+// If you hover over faker and press the cmd key if on Mac (ctrl - Windows) then it will take you to the 'type definition file' for faker.
+
+// Inside this file, you're going to find nothing but typescript syntax that describes the types of values, functions, objects and classes inside the faker library.
