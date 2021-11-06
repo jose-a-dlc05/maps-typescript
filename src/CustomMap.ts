0848bec60dc0ef280ec262742200fe37fe96227f
Jose De La Cruz <jose.delcruz0490@icloud.com>
2021-11-06 18:23:50 -0400

    Made addMarker function inside of CustomMap file dynamic

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,3 +1,13 @@
+// Instructions to every other class
+// on how to be an argument to 'addMarker'
+// So long as a class has a location object with lat and lng properties
+// then it fulfills the interface Mappable
+interface Mappable {
+	location: {
+		lat: number;
+		lng: number;
+	};
+}
 export class CustomMap {
 	private googleMap: google.maps.Map;
 
@@ -7,6 +17,18 @@ export class CustomMap {
 			center: {
 				lat: 0,
 				lng: 0,
+			},
+		});
+	}
+
+	addMarker(mappable: Mappable): void {
+		// Mappable argument will only look at properties intersecting in both types
+		// Typescript limits this
+		new google.maps.Marker({
+			map: this.googleMap,
+			position: {
+				lat: mappable.location.lat,
+				lng: mappable.location.lng,
 			},
 		});
 	}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 /// <reference types="@types/google.maps" />
-import { User } from './User';
 import { Company } from './Company';
 import { CustomMap } from './CustomMap';
+import { User } from './User';
 
-new CustomMap('map');
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap('map');
+
+customMap.addMarker(user);
+customMap.addMarker(company);
